b2a6caf8e3f124dacb8ae1b73cdb6f4ebf5a3dc9
piergiorgio.roveda <piergiorgio.roveda@cityplanner.biz>
2024-09-02 09:16:01 +0000

    Update

--- template-map-sidebar/index.php
+++ template-map-sidebar/index.php
@@ -1,4 +1,9 @@
 <?php 
+
+error_reporting(E_ALL);
+ini_set('display_errors', 'on');
+// ini_set('memory_limit', '200M');
+
 require '/mnt/volume_fra1_01/cityplanner.biz/env-h3hkngbjrg69bfje9sz8fm2u.php';
 require '/mnt/volume_fra1_01/cityplanner.biz/wp-config-extends.php';
 require './public/page.php';
