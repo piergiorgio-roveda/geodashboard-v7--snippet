7b9c695108e7e7bf6f421f7c959ce06455da288a
piergiorgio.roveda <piergiorgio.roveda@cityplanner.biz>
2024-09-02 09:17:22 +0000

    Update

--- template-map-sidebar/public/page.php
+++ template-map-sidebar/public/page.php
@@ -2,6 +2,7 @@
 
 define('BUCKET', 'https://geo-source.ams3.digitaloceanspaces.com/');
 define('THUMB', BUCKET.'screenshoots/24-070809/banner-geodashboard.png');
+define('AUTHOR__NAME', BUCKET.'Piergiorgio Roveda');
 $_page_attributes = array(
   "page_lang"=>"en-US",
   "title_seo"=>"Template Map Sidebar",
