869adcfd6abfe4c673b42195f4b53fbba613cde8
piergiorgio.roveda <piergiorgio.roveda@cityplanner.biz>
2024-09-02 09:56:59 +0000

    Update

--- template-map-sidebar/public/index_template.php
+++ template-map-sidebar/public/index_template.php
@@ -14,15 +14,30 @@
     </div>
   </div>
   <div class="sidebar__body">
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed cursus a lacus et posuere. Morbi posuere nulla nunc, at faucibus massa fermentum et. Cras quis dolor at ante placerat dictum. Nulla ut tempus nulla, id scelerisque nisi. Nam bibendum tortor venenatis felis pretium facilisis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Cras et finibus nisi. Nam quis ipsum maximus, ultricies est imperdiet, lacinia lacus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Ut sollicitudin fringilla dignissim. Vestibulum finibus urna id lectus lacinia, eget mattis lacus cursus. Ut a quam non augue viverra lacinia.
-
-    Mauris egestas volutpat nulla, ac tristique sem convallis ut. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Pellentesque cursus eros eget nisi pharetra, a tincidunt justo congue. Donec eleifend quis justo non fringilla. Donec facilisis nisi in sem lobortis ultrices. Curabitur vitae euismod dolor. Suspendisse potenti. Pellentesque scelerisque faucibus sapien, id pretium ligula rhoncus eu.
-
-    Vestibulum turpis quam, dapibus sed dignissim eget, porttitor vitae ex. Curabitur venenatis ipsum mi, et luctus lacus aliquet at. Etiam imperdiet iaculis felis, a ultrices lorem. Sed accumsan leo eu dapibus eleifend. Quisque facilisis gravida pharetra. Donec sed nisi id odio congue scelerisque at nec dolor. Nullam fringilla rhoncus libero sed tincidunt. Donec cursus massa nec arcu aliquam iaculis. Etiam non varius dui. Proin in felis lectus.
-
-    Praesent quam eros, convallis sed enim a, sollicitudin accumsan diam. Phasellus libero metus, posuere sit amet augue et, ullamcorper condimentum metus. Etiam ut nibh iaculis, aliquam dolor ac, tempor dui. Integer ut auctor erat. Nullam iaculis odio vel velit commodo dictum. In libero justo, porttitor at ligula fringilla, viverra accumsan augue. Praesent et ultricies justo. Curabitur a justo ut massa venenatis lacinia. Nulla vitae nibh non nunc consequat blandit. Maecenas eleifend ut ligula sed ornare. Curabitur fermentum eu risus vitae cursus. Nam enim turpis, molestie at orci vel, varius facilisis diam. Vivamus sed augue diam. Donec rhoncus dui a tortor tincidunt, non lobortis ante venenatis. Integer malesuada, lorem pharetra sagittis semper, lacus ex sagittis sem, a rhoncus ante leo id nulla. Curabitur ultricies velit elit, id sodales dolor placerat at.
-
-    Mauris tempor ex nec faucibus hendrerit. Mauris aliquam urna at odio pretium pharetra. Mauris bibendum erat dictum tempor dignissim. Nullam congue ante sit amet tempus consectetur. Nullam malesuada, lorem non dapibus ullamcorper, nibh eros faucibus risus, quis volutpat nisi urna a purus. Mauris mollis libero sit amet tellus mattis, ut pharetra mauris facilisis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Sed semper blandit diam non rutrum. Phasellus ac enim odio. Aenean pellentesque libero vitae elit feugiat euismod finibus vel libero. Maecenas feugiat dui eget leo egestas, et aliquet tortor luctus. Sed faucibus ut lorem eget iaculis. Etiam dictum laoreet tempus.
+    <?php
+      // Loop to create 10 elements
+      for ($i = 1; $i <= 10; $i++) {
+        echo '
+          <div class="tocbox" 
+            geodata_output__pid="lyr_'.$i.'"
+              >
+            <div class="tocbox__check" >
+              <input type="checkbox" 
+                class="form-check-input tocbox__check__input" 
+                value="" 
+                geodata_output__pid="lyr_'.$i.'" checked >
+            </div>
+            <div class="tocbox__label" >
+              Layer '.$i.'
+            </div>
+            <div class="tocbox__expand"  
+              geodata_output__pid="lyr_'.$i.'" >
+              <i style="display:none;" class="bi bi-chevron-expand"></i>
+            </div>
+          </div>
+        ';
+      }
+    ?>
   </div>
   <div class="sidebar__footer">
     <div class="btn-group" role="group" aria-label="Basic example" style="width: 100%;">
